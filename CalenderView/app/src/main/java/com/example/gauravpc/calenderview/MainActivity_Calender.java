774e51ccf47783d0a5f6a79278e89c4efe3061d0
gaurav sharma <gauravsharma.mvp@gmail.com>
2016-06-22 21:15:52 +0530

    Calender View Initial Commit

---
+++ CalenderView/app/src/main/java/com/example/gauravpc/calenderview/MainActivity_Calender.java
@@ -0,0 +1,31 @@
+package com.example.gauravpc.calenderview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class MainActivity_Calender extends AppCompatActivity {
+
+    CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity__calender);
+
+        calendarView=(CalendarView)findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(),dayOfMonth+"/"+month+"/"+year,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
